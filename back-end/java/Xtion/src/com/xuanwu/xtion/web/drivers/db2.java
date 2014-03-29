6e1daf3f0f316c6e940907570cd318610ba9e8f7
changyingwei <weichangying@msn.com>
2014-03-29 16:41:58 +0800

    update

--- back-end/java/Xtion/src/com/xuanwu/xtion/web/drivers/db2.java
+++ back-end/java/Xtion/src/com/xuanwu/xtion/web/drivers/db2.java
@@ -1,3 +1,8 @@
+/*
+ *author:weichangying
+ *date:2014-03-29
+ *remark:the dirver for db2 database.
+ */
 package com.xuanwu.xtion.web.drivers;
 
 public class db2 {
--- back-end/java/Xtion/src/com/xuanwu/xtion/web/drivers/pgsql.java
+++ back-end/java/Xtion/src/com/xuanwu/xtion/web/drivers/pgsql.java
@@ -1,3 +1,8 @@
+/*
+ *author:weichangying
+ *date:2014-03-29
+ *remark:the dirver for postgresql database.
+ */
 package com.xuanwu.xtion.web.drivers;
 
 public class pgsql {
